30cb20ce80783f845c5da916ee6a08b103c67b7a
longtongster <sacha.van.weeren@gmail.com>
2020-06-03 09:22:19 +0200

    Organized the tests related to MyDataReader in a Test class

--- my_package/reader_module/test_reader.py
+++ my_package/reader_module/test_reader.py
@@ -7,8 +7,22 @@ Created on Mon Jun  1 09:36:09 2020
 In order to run pytest from the my_full_package directory run the command:
 !pytest ./my_package/reader_module/test_reader.py
 
+Run !pytest from the my_full_package directory to run all tests in the
+directories
+
 Or run the command from the directory this file is in
 !pytest test_reader.py
+
+To run specific tests use
+!pytest test_module.py 
+!pytest test_module.py::TestClass 
+!pytest test_module.py::TestClass::test_function 
+
+Files to test must start with 'test_'. A test class must start with the name
+'Test'. A test function needs to start with 'test_'
+
+Flags:
+    - x : pytest stops the first time it finds a failure
 
 1. store a test module in same dir as the module you want to test
 2. by convention call module test_<module name>
@@ -19,6 +33,8 @@ tips:
 - use isinstance to check data types
 - use pytest.raises() to test for desired Error messages 
 - do tests with bad data, bounderies and normal cases (2 to 3 for each case)
+- gather all tests related to a function in oneclass that starts with the function
+name followed by the function name.
 - TDD - Test driven development - write tests before the functions are written
     - in this case we factor in the time to write tests
 
@@ -31,65 +47,71 @@ import pytest
 from reader import MyDataReader
 from collections import OrderedDict
 
-def test_dictreader_two_simple_rows():
-    # create simple test file
-    path = "test.txt"
-    with open(path,'w') as f:
-        f.write("header1\theader\theader3\n")
-        f.write("test11\ttest12\ttest13\n")
-        f.write("test21\ttest22\ttest23")
+class TestMyDataReader(object):
+    """
+    Test class to test the MyDataReader class
+    """
     
-    test_reader = MyDataReader(path)
-    test_reader.DictReader()
-    actual = test_reader.data
-    expected =[OrderedDict([('header1', 'test11'), ('header', 'test12'),('header3', 'test13')]), 
-               OrderedDict([('header1', 'test21'), ('header', 'test22'), ('header3', 'test23')])]   
-    message = "MyDataReader.data returned {0} instead of {1}".format(actual,expected)     
-    assert actual == expected, message
+    def test_dictreader_two_simple_rows(self):
+        # create simple test file
+        path = "test.txt"
+        with open(path,'w') as f:
+            f.write("header1\theader\theader3\n")
+            f.write("test11\ttest12\ttest13\n")
+            f.write("test21\ttest22\ttest23")
+    
+        test_reader = MyDataReader(path)
+        test_reader.DictReader()
+        actual = test_reader.data
+        expected =[OrderedDict([('header1', 'test11'), ('header', 'test12'),('header3', 'test13')]), 
+                   OrderedDict([('header1', 'test21'), ('header', 'test22'), ('header3', 'test23')])]   
+        message = "MyDataReader.data returned {0} instead of {1}".format(actual,expected)     
+        assert actual == expected, message
 
-def test_csvreader_two_simple_rows():
-    # create simple test file
-    path = "test.txt"
-    with open(path,'w') as f:
-        f.write("header1\theader\theader3\n")
-        f.write("test11\ttest12\ttest13\n")
-        f.write("test21\ttest22\ttest23")
+
+    def test_csvreader_two_simple_rows(self):
+        # create simple test file
+        path = "test.txt"
+        with open(path,'w') as f:
+            f.write("header1\theader\theader3\n")
+            f.write("test11\ttest12\ttest13\n")
+            f.write("test21\ttest22\ttest23")
 
     
-    test_reader = MyDataReader(path)
-    test_reader.csvReader()
-    actual = test_reader.data
-    expected = [['test11', 'test12', 'test13'], 
-                ['test21', 'test22', 'test23']]
-    message = "MyDataReader.data returned {0} instead of {1}".format(actual, expected)
-    assert actual == expected , message   
+        test_reader = MyDataReader(path)
+        test_reader.csvReader()
+        actual = test_reader.data
+        expected = [['test11', 'test12', 'test13'], 
+                    ['test21', 'test22', 'test23']]
+        message = "MyDataReader.data returned {0} instead of {1}".format(actual, expected)
+        assert actual == expected , message   
             
-def test_mysum_clean():
-    # this test case shows how to make unit test for float variables
-    x = 0.1 + 0.1
-    y = 0.1
-    path=None
-    test_reader = MyDataReader(path)
-    test_reader.MySum(x,y)
-    actual = test_reader.total
-    # pytest.approx() will also work on lists and numpy arrays
-    expected = pytest.approx(0.3)
-    message = "MyDataReader.MySum returned {0} instead of {1}".format(actual, expected)
-    assert actual == expected , message  
-    assert isinstance(actual, float), f"expected datatype is float"
+    def test_mysum_clean(self):
+        # this test case shows how to make unit test for float variables
+        x = 0.1 + 0.1
+        y = 0.1
+        path=None
+        test_reader = MyDataReader(path)
+        test_reader.MySum(x,y)
+        actual = test_reader.total
+        # pytest.approx() will also work on lists and numpy arrays
+        expected = pytest.approx(0.3)
+        message = "MyDataReader.MySum returned {0} instead of {1}".format(actual, expected)
+        assert actual == expected , message  
+        assert isinstance(actual, float), f"expected datatype is float"
     
     
-def test_mysum_error():
-    x = 10
-    y = "string"
-    path=None
+    def test_mysum_error(self):
+        x = 10
+        y = "string"
+        path=None
     
-    test_reader = MyDataReader(path)
-    with pytest.raises(TypeError) as exc_info:
+        test_reader = MyDataReader(path)
+        with pytest.raises(TypeError) as exc_info:
             test_reader.MySum(x,y)
-    #I am not able to get the error messgae to work. I might do something wrong
-    #message = """unsupported operand type(s) for +: 'int' and 'str'"""
-    #print("ERROR MESSAGE",exc_info.value)
-    #assert exc_info.match(message)
+        #I am not able to get the error messgae to work. I might do something wrong
+        #message = """unsupported operand type(s) for +: 'int' and 'str'"""
+        #print("ERROR MESSAGE",exc_info.value)
+        #assert exc_info.match(message)
     
     
