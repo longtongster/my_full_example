28dcd01249754b7d04d0db2c5e7267a53ecf8978
sacha.van.weeren <sacha.van.weeren@gmail.com>
2020-09-26 08:48:36 +0200

    Improved comments to explain the import is based on relative directory the module is in

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -5,6 +5,9 @@ Created on Sun May 31 21:55:20 2020
 @author: sacha
 """
 
+# Using imports in the init file requires a relative path
+# Here we have the reader module in the reader_module directory
+# so we have to use .reader_module.reader
 from .reader_module.reader import hoi
 from .reader_module.reader import MyDataReader
 
